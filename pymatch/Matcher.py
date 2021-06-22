e270f7c39d8dceeec502e49b0e6a24434ac3dd2f
Shivaprasad Patil <shivaprasad309319@gmail.com>
2021-06-22 06:02:00 +0200

    Update Matcher.py

--- pymatch/Matcher.py
+++ pymatch/Matcher.py
@@ -299,7 +299,8 @@ class Matcher:
         """
         test_results = []
         for col in self.matched_data.columns:
-            if uf.is_continuous(col, self.X) and col not in self.exclude:
+            is_continuous = (col in self.X.columns) or ("Q('{}')".format(col) in self.X.columns)
+            if is_continuous and (not(col in self.exclude)):
                 # organize data
                 trb, cob = self.test[col], self.control[col]
                 tra = self.matched_data[self.matched_data[self.yvar]==True][col]
@@ -405,7 +406,8 @@ class Matcher:
         '''
         test_results = []
         for col in self.matched_data.columns:
-            if not uf.is_continuous(col, self.X) and col not in self.exclude:
+            is_continuous = (col in self.X.columns) or ("Q('{}')".format(col) in self.X.columns)
+            if (not is_continuous) and (not(col in self.exclude)):
                 dbefore = prep_plot(self.data, col, colname="before")
                 dafter = prep_plot(self.matched_data, col, colname="after")
                 df = dbefore.join(dafter)
